2556523c4a98ef0e54b30a40782a17407d122164
poseidon98 <bickrikhidam@gmail.com>
2022-01-10 11:02:04 +0700

    new side-bar

--- resources/views/components/side-Bar.blade.php
+++ resources/views/components/side-Bar.blade.php
@@ -24,34 +24,7 @@
           Interface
       </div> -->
 
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-              aria-controls="collapseTwo">
-              <i class="fas fa-fw fa-cog"></i>
-              <span>Profil</span>
-          </a>
-          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-              <div class="py-2 bg-white rounded collapse-inner">
-                  <a class="collapse-item" href="{{ url('/dashboard') }}">Data Pribadi</a>
-              </div>
-          </div>
-      </li>
 
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-              aria-expanded="true" aria-controls="collapseUtilities">
-              <i class="fas fa-fw fa-wrench"></i>
-              <span>Pelaks.Pendidikan</span>
-          </a>
-          <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-              data-parent="#accordionSidebar">
-              <div class="bg-white py-2 collapse-inner rounded">
-                  <a class="collapse-item" href="{{ url('/pddikti/pengajaran') }}">Pengajaran</a>
-              </div>
-          </div>
-      </li>
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
