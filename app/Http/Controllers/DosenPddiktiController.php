84dfa18be1048c009bfa74c498b68b374281d367
poseidon98 <bickrikhidam@gmail.com>
2022-01-10 11:35:18 +0700

    dosen dikti

--- app/Http/Controllers/DosenPddiktiController.php
+++ app/Http/Controllers/DosenPddiktiController.php
@@ -12,13 +12,13 @@ class DosenPddiktiController extends Controller
     public function index()
     {
         $AmbilData = PddiktiPengajaran::join('daftar_dosen_diktis', 'daftar_dosen_diktis.id_pengajaran_pddikti', '=', 'pengajaran_pddikti.id_pengajaran_pddikti')
-            ->join('data_dosen', 'data_dosen.kode_dosen', '=', 'daftar_dosen_diktis.dosen')
+            ->join('daftar_dosen_nidn', 'daftar_dosen_nidn.no_registrasi', '=', 'daftar_dosen_diktis.dosen')
             ->groupBy('pengajaran_pddikti.id_pengajaran_pddikti')
             ->groupBy('pengajaran_pddikti.nama_matkul')
             ->groupBy('pengajaran_pddikti.sks_asli')
             ->groupBy('pengajaran_pddikti.akademik_tahun')
-            ->groupBy('data_dosen.nama_dosen')
-            ->selectRaw('pengajaran_pddikti.id_pengajaran_pddikti, pengajaran_pddikti.nama_matkul, pengajaran_pddikti.sks_asli, pengajaran_pddikti.akademik_tahun, data_dosen.nama_dosen')
+            ->groupBy('daftar_dosen_nidn.nama')
+            ->selectRaw('pengajaran_pddikti.id_pengajaran_pddikti, pengajaran_pddikti.nama_matkul, pengajaran_pddikti.sks_asli, pengajaran_pddikti.akademik_tahun, daftar_dosen_nidn.nama')
             ->where('pengajaran_pddikti.prodi_id', Auth::user()->prodi_id)
             ->get();
 
@@ -44,12 +44,12 @@ class DosenPddiktiController extends Controller
     public function detail($id)
     {
         $detail_data = PddiktiPengajaran::join('daftar_dosen_diktis', 'daftar_dosen_diktis.id_pengajaran_pddikti', '=', 'pengajaran_pddikti.id_pengajaran_pddikti')
-            ->join('data_dosen', 'data_dosen.kode_dosen', '=', 'daftar_dosen_diktis.dosen')
+            ->join('daftar_dosen_nidn', 'daftar_dosen_nidn.no_registrasi', '=', 'daftar_dosen_diktis.dosen')
             ->where('pengajaran_pddikti.prodi_id', Auth::user()->prodi_id)
             ->where('pengajaran_pddikti.id_pengajaran_pddikti', $id)
             ->first();
 
-        $anggota = DaftarDosenDikti::join('data_dosen', 'data_dosen.kode_dosen', '=', 'daftar_dosen_diktis.dosen_anggota')
+        $anggota = DaftarDosenDikti::join('daftar_dosen_nidn', 'daftar_dosen_nidn.no_registrasi', '=', 'daftar_dosen_diktis.dosen')
             ->where('id_pengajaran_pddikti', $id)
             ->get();
 
